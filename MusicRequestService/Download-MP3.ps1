4d5ab1bc4529519d90f1769eca720723466df503
Florian Tischler <florian.tischler@outlook.com>
2018-05-09 22:18:18 +0200

    fix target path by using System.IO.Path::Combine assume ffmpeg to be on path

--- MusicRequestService/Download-MP3.ps1
+++ MusicRequestService/Download-MP3.ps1
@@ -1,3 +1,5 @@
 param([string] $URL, [string] $TargetFolder)
 
-& "youtube-dl" --ffmpeg-location C:\ffmpeg-20171111-3af2bf0-win64-static\bin --extract-audio --audio-format mp3 -o "$TargetFolder\%(title)s.%(ext)s" -f "bestaudio[height <=? 720]/best[height <=? 720]/best" $URL 
+$outputTemplate = [System.IO.Path]::Combine($TargetFolder, "%(title)s.%(ext)s")
+
+& "youtube-dl" --extract-audio --audio-format mp3 -o $outputTemplate -f "bestaudio[height <=? 720]/best[height <=? 720]/best" $URL 
