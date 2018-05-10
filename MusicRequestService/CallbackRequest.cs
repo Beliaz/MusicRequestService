36bb9362cee912513bafe34e21dae72bc5a0cde6
Florian Tischler <florian.tischler@outlook.com>
2018-05-10 22:49:36 +0200

    refactoring, code cleanup

---
+++ MusicRequestService/CallbackRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace MusicRequestService
+{
+    [DataContract]
+    public class CallbackRequest
+    {
+        [DataMember]
+        public string Uri { get; set; }
+
+        [DataMember]
+        public string FileName { get; set; }
+    }
+}
--- MusicRequestService/TextPlainInputFormatter.cs
+++ MusicRequestService/TextPlainInputFormatter.cs
@@ -21,12 +21,10 @@ namespace MusicRequestService
 
         public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context, Encoding encoding)
         {
-            string data = null;
             using (var streamReader = context.ReaderFactory(context.HttpContext.Request.Body, encoding))
             {
-                data = await streamReader.ReadToEndAsync();
+                return InputFormatterResult.Success(await streamReader.ReadToEndAsync());
             }
-            return InputFormatterResult.Success(data);
         }
     }
 }
