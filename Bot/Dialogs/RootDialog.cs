5a9fcebcd7f47f08cf8cfa90ef3f6478ec15b2e8
Florian Tischler <florian.tischler@outlook.com>
2018-06-07 14:04:08 +0200

    added bot with luis to detect "play song" and "get current song" intents

---
+++ Bot/Dialogs/RootDialog.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Luis;
+using System.Configuration;
+using System.Linq;
+using Microsoft.Bot.Builder.Luis.Models;
+
+namespace Bot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : LuisDialog<object>
+    {
+        public RootDialog() : base(new LuisService(new LuisModelAttribute(
+            ConfigurationManager.AppSettings["LuisAppId"],
+            ConfigurationManager.AppSettings["LuisAPIKey"],
+            domain: ConfigurationManager.AppSettings["LuisAPIHostName"])))
+        {
+        }
+
+        [LuisIntent("None")]
+        public async Task NoneIntent(IDialogContext context, LuisResult result)
+        {
+            await ShowLuisResult(context, result);
+        }
+
+        [LuisIntent("PlaySong")]
+        public async Task PlayIntent(IDialogContext context, LuisResult result)
+        {
+            await ShowLuisResult(context, result);
+        }
+
+        [LuisIntent("GetCurrentSong")]
+        public async Task CurrentSongIntent(IDialogContext context, LuisResult result)
+        {
+            await ShowLuisResult(context, result);
+        }
+
+        private async Task ShowLuisResult(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"You have reached {result.Intents[0].Intent}. You said: {result.Query}");
+            context.Wait(MessageReceived);
+        }
+    }
+}
