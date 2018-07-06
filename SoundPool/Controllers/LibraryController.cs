1fb1946b991e23abd5c8a3b5ffd29231085d839a
Florian Tischler <florian.tischler@outlook.com>
2018-07-06 13:50:21 +0200

    add GetSong(id) and GetArtist(id)

--- SoundPool/Controllers/LibraryController.cs
+++ SoundPool/Controllers/LibraryController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using SoundPool.Data;
 using SoundPool.Data.EFCore;
 
 namespace SoundPool.Controllers
@@ -24,12 +26,26 @@ namespace SoundPool.Controllers
                 .Include(s => s.Artist));
         }
 
+        [HttpGet("Songs/{id}")]
+        public ActionResult<IEnumerable<string>> GetSong(string id)
+        {
+            return Ok(_dbContext
+                .Songs.Where(s => s.Id.Equals(id))
+                .Include(s => s.Artist)
+                .First());
+        }
+
         [HttpGet("Artists")]
         public ActionResult<IEnumerable<string>> GetArtists()
         {
-            return Ok(_dbContext
-                .Songs
-                .Include(s => s.Artist));
+            return Ok(_dbContext.Artists);
+        }
+
+        [HttpGet("Artists/{id}")]
+        public ActionResult<IEnumerable<string>> GetArtist(string id)
+        {
+            return Ok(_dbContext.Artists
+                .First(s => s.Id.Equals(id)));
         }
     }
 }
